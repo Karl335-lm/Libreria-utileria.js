66081cd0a6f9af62f5a70e9f83d66365fa75c9bc
Karl335-lm <karlamartinezbautista25@gmail.com>
2026-09-23 22:24:06 -0600

    Agregar js

--- js/utilleria.js
+++ js/utilleria.js
@@ -1,1 +1,60 @@
-kddmskd;
+%Funciones 
+%Validar correo
+function validarCorreo(correo) {
+    const patron =  /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return patron.test(correo);   
+}
+
+%soloLetras
+function soloLetras(texto) {
+    const patron =  /^[A-Za-záéíóúÑñúÁÉÍÓÚüÜ]/;
+    return patron.test(texto);   
+}
+
+%longitud
+function validarLongitud(numero, maxLongitud) {
+    return String(numero).length <= maxLongitud;
+}
+
+%edad
+function calcularEdad(fechaNacimiento) {
+
+    const nacimiento = new Date(fechaNacimiento + "T00:00:00");
+    const hoy = new Date();
+
+    let edad = hoy.getFullYear() - nacimiento.getFullYear();
+
+    const mes = hoy.getMonth() - nacimiento.getMonth();
+
+    if (
+        mes < 0 ||
+        (mes === 0 && hoy.getDate() < nacimiento.getDate())
+    ) {
+        edad--;
+    }
+
+    return edad;
+}
+
+%mayoria de edad
+function esMayorDeEdad(fechaNacimiento) {
+    return calcularEdad(fechaNacimiento) >= 18;
+}
+
+%validar contraseña
+function validarPassword(password) {
+
+    const tieneMayuscula = /[A-Z]/.test(password);
+    const tieneMinuscula = /[a-z]/.test(password);
+    const tieneNumero = /[0-9]/.test(password);
+    const tieneEspecial = /[^A-Za-z0-9]/.test(password);
+    const tieneLongitud = password.length >= 8;
+
+    return (
+        tieneMayuscula &&
+        tieneMinuscula &&
+        tieneNumero &&
+        tieneEspecial &&
+        tieneLongitud
+    );
+}
